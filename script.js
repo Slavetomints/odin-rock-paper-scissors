8cdc71e5ba37339f50fa3bff1ec93b6aa835a30f
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2024-05-13 16:29:00 -0500

    added a basic UI

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let computerScore = 0;
+let humanScore = 0;
+let buttons = document.querySelector('#buttons');
+let winner = document.querySelector('#winner');
+
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
     let computerChoice;
@@ -8,58 +13,54 @@ function getComputerChoice() {
     } else {
         computerChoice = "scissors";
     }
-    return computerChoice
+    return computerChoice;
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Pick either 'rock', 'paper', or 'scissors'").toLowerCase();
-    return humanChoice
-}
+function playRound() {
+    const results = document.querySelector("#results");
+    computerChoice = getComputerChoice();
 
-function playGame() {
-
-    function playRound(computerChoice, humanChoice) {
-
-        computerChoice = getComputerChoice();
-        humanChoice = getHumanChoice();
         
-        if (humanChoice === computerChoice) {
-            console.log(`You tie! ${humanChoice} ties with ${computerChoice}`);
-        } else if ((humanChoice === 'rock' && computerChoice === 'paper') || (humanChoice === 'scissors' && computerChoice === 'rock') || (humanChoice === 'paper' && computerChoice === 'scissors')) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
-            ++computerScore;
-        } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-            ++humanScore;
-        }
+    if (humanChoice === computerChoice) {
+            results.textContent = `You tie! ${humanChoice} ties with ${computerChoice}`;
+    } else if ((humanChoice === 'rock' && computerChoice === 'paper') || 
+               (humanChoice === 'scissors' && computerChoice === 'rock') || 
+               (humanChoice === 'paper' && computerChoice === 'scissors')) {
+        results.textContent = `You lose! ${computerChoice} beats ${humanChoice}!`;
+        ++computerScore;
+    } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || 
+               (humanChoice === 'paper' && computerChoice === 'rock') || 
+               (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        results.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
+        ++humanScore;
     }
-
-    let computerScore = 0;
-    let humanScore = 0;
-    
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-
-    while (humanScore === computerScore) {
-        playRound();
-        if (humanScore > computerScore) {
-            console.log('You win! :D');
-            break
-        } else if (humanScore < computerScore) {
-            console.log('you lose :(');
-            break
-        } else {
-            playRound();
-        }
-    } 
-    if (humanScore > computerScore) {
-        console.log('You win! :D');
+    if (humanScore >= 5 && computerScore < 5) {
+        winner.textContent = 'You win! :D';
+        winner.style.backgroundColor = 'yellow'
+    } else if (computerScore >= 5 && humanScore < 5) {
+        winner.textContent = 'you lose :(';
+        winner.style.backgroundColor = 'yellow'
+    } else if (humanScore >= 5 && computerScore >= 5) {
+        winner.textContent = 'reload the page to play again'
+        winner.style.backgroundColor = 'red'
     } else {
-        console.log('you lose :(');
+        winner.textContent = 'play more games!'
     }
 }
 
-playGame()
+buttons.addEventListener('click', (event) => {
+    let target = event.target;
+    
+    switch(target.id) {
+        case 'rock':
+            humanChoice = 'rock';
+            break;
+        case 'paper':
+            humanChoice = 'paper';
+            break;
+        case 'scissors':
+            humanChoice = 'scissors';
+            break;
+    }
+    playRound()
+});
